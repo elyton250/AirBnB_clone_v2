88647a773e8a1f0d701c45c6990e378b695ee305
elyton250 <nelysemathieu@gmail.com>
2024-02-16 17:57:26 +0200

    webflask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this routes to the other path"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    """thsi displays hello"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """this rerouts to hbnb"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this module i added the c display"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_diplay(text):
+    """this display c and text"""
+    modified_text = text.replace('_', ' ')
+    return f"c {modified_text}"
+
+@app.route('/python/<text>')
+def python_diplay(text):
+    """this display c and text"""
+    if text is not None:
+        modified_text = text.replace('_', ' ')
+        return f"Python {modified_text}"
+    else: 
+        text = "is cool"
+        return f"Python {text}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
