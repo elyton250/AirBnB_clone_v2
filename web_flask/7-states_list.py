c39e9959eae5f6132a725b44a465af9700411a1d
elyton250 <nelysemathieu@gmail.com>
2024-02-18 17:22:37 +0200

    solved the import issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""this modules impliments falsk"""
-from flask import Flask, render_template
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes
+"""
+import dir_solv
+from models.state import State
 from models import storage
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/states_list')
-def display_states():
-    """This function renders a template with a list of states"""
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current sqalchemy session"""
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
---
+++ web_flask/dir_solv.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""this modules solve the import issue"""
+import sys
+from os.path import abspath, dirname
+sys.path.append(dirname(dirname(abspath(__file__))))
