6018bf42081fd0f2af8249f77b256508974ab3d9
elyton250 <nelysemathieu@gmail.com>
2024-02-18 20:55:13 +0200

    task 223

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""this module i added the c display"""
+"""this modules returns a number"""
+
+from flask import Flask, render_template
 
 
-from flask import Flask
+def create_app():
+    """this creates the app"""
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+    @app.route('/')
+    def hello_world():
+        """ Returns some text. """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb')
+    def hello():
+        """ Return other text. """
+        return 'HBNB'
+
+    @app.route('/c/<text>')
+    def c_text(text):
+        """ replace text with variable. """
+        text = text.replace('_', ' ')
+        return 'C {}'.format(text)
+    return app
 
 
-@app.route('/')
-def hello():
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb')
-def hello_hbnb():
-    return "HBNB"
-
-
-@app.route('/c/<text>')
-def c_diplay(text):
-    """this display c and text"""
-    modified_text = text.replace('_', ' ')
-    return f"c {modified_text}"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    create_app().run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-""" edirects and has default val for variable """
+"""this modules returns a number"""
 
-from flask import Flask
-
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+from flask import Flask, render_template
 
 
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+def create_app():
+    """this creates the app"""
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
+    @app.route('/')
+    def hello_world():
+        """ Returns some text. """
+        return 'Hello HBNB!'
 
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
+    @app.route('/hbnb')
+    def hello():
+        """ Return other text. """
+        return 'HBNB'
 
+    @app.route('/c/<text>')
+    def c_text(text):
+        """ replace text with variable. """
+        text = text.replace('_', ' ')
+        return 'C {}'.format(text)
 
-@app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+    @app.route('/python/')
+    @app.route('/python/<text>')
+    def python_text(text='is cool'):
+        """ replace more text with another variable. """
+        text = text.replace('_', ' ')
+        return 'Python {}'.format(text)
 
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+    @app.route('/number/<int:n>')
+    def number_text(n):
+        """ replace with int only if given int. """
+        n = str(n)
+        return '{} is a number'.format(n)
+    return app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    create_app().run(host='0.0.0.0', port=5000)
