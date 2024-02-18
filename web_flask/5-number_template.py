06957193a733bd18eee02f94fe2b9962b6fe7d07
elyton250 <nelysemathieu@gmail.com>
2024-02-18 20:46:46 +0200

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,60 +1,45 @@
-#!/usr/bin/python3
-"""this module i added the c display"""
-
-
 from flask import Flask, render_template
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+def create_app():
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
+    @app.route('/')
+    def hello_world():
+        """ Returns some text. """
+        return 'Hello HBNB!'
 
-@app.route('/')
-def hello():
-    return "Hello HBNB!"
+    @app.route('/hbnb')
+    def hello():
+        """ Return other text. """
+        return 'HBNB'
 
+    @app.route('/c/<text>')
+    def c_text(text):
+        """ replace text with variable. """
+        text = text.replace('_', ' ')
+        return 'C {}'.format(text)
 
-@app.route('/hbnb')
-def hello_hbnb():
-    return "HBNB"
+    @app.route('/python/')
+    @app.route('/python/<text>')
+    def python_text(text='is cool'):
+        """ replace more text with another variable. """
+        text = text.replace('_', ' ')
+        return 'Python {}'.format(text)
 
+    @app.route('/number/<int:n>')
+    def number_text(n):
+        """ replace with int only if given int. """
+        n = str(n)
+        return '{} is a number'.format(n)
 
-@app.route('/c/<text>')
-def c_diplay(text):
-    """this display c and text"""
-    modified_text = text.replace('_', ' ')
-    return f"c {modified_text}"
+    @app.route('/number_template/<int:n>')
+    def html_num(n):
+        """ display html if n is int. """
+        n = str(n)
+        return render_template('5-number.html', n=n)
 
+    return app
 
-@app.route('/python/<text>')
-def python_diplay(text):
-    """this display c and text"""
-    if text is not None:
-        modified_text = text.replace('_', ' ')
-        return f"Python {modified_text}"
-    else:
-        text = "is cool"
-        return f"Python {text}"
-
-
-@app.route('/number/<n>')
-def display_number(n):
-    try:
-        n_int = int(n)
-        return f"{n_int} is a number"
-    except ValueError:
-        return f"{n} is not a valid number"
-
-
-@app.route('/number_template/<n>')
-def html_display(n):
-    """display html"""
-    try:
-        n_int = int(n)
-        if isinstance(n_int, int):
-            return render_template('5-number.html', n=n_int)
-    except ValueError:
-        return f"{n} is not a valid number"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    create_app().run(host='0.0.0.0', port=5000)
