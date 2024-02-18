b0175e238ce78867ca6eebd410483052f2af419b
elyton250 <nelysemathieu@gmail.com>
2024-02-18 20:36:32 +0200

    task2 done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""this module i added the c display"""
-
+""" edirects and has default val for variable """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
-    return "Hello HBNB!"
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hello_hbnb():
-    return "HBNB"
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_diplay(text):
-    """this display c and text"""
-    modified_text = text.replace('_', ' ')
-    return f"C {modified_text}"
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
+@app.route('/python/')
 @app.route('/python/<text>')
-def python_diplay(text):
-    """this display c and text"""
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
